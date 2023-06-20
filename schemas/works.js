f3357b4473c2bb2acf29491050c77765182b87f0
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-06-20 18:06:22 +0500

    edited work schema

--- schemas/works.js
+++ schemas/works.js
@@ -1,50 +1,76 @@
 export default {
-    name: 'works',
-    title: 'Works',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
+  name: 'works',
+  title: 'Works',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'projectLink',
+      title: 'Project Link',
+      type: 'string',
+    },
+    {
+      name: 'codeLink',
+      title: 'Code Link',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImageUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-    
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'string',
-      },
-      {
-        name: 'projectLink',
-        title: 'Project Link',
-        type: 'string',
-      },
-      {
-        name: 'codeLink',
-        title: 'Code Link',
-        type: 'string',
-      },
-      {
-        name: 'imgUrl',
-        title: 'ImageUrl',
-        type: 'image',
-        options: {
-          hotspot: true,
+    },
+
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
         },
-      },
-   
-      {
-        name: 'tags',
-        title: 'Tags',
-       type:'array',
-       of: [
-         {
-           name:'tag',
-           title:'Tag',
-           type:'string'
-         }
-       ]
-      },
-     
-    ],
-  };
+      ],
+    },
+    {
+      name: 'techs',
+      title: 'Techs',
+      type: 'array',
+      of: [
+        {
+          name: 'tech',
+          title: 'Tech',
+          type: 'document',
+          fields: [
+            {
+              name: 'tag',
+              title: 'Tag',
+              type: 'string',
+            },
+            {
+              name: 'icon',
+              title: 'Icon',
+              type: 'image',
+              options: {
+                hotspot: true,
+              }
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
